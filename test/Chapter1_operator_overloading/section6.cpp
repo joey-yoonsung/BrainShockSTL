c3bdeaaa073a15f98d97fe0cd55e9eff2c8ac0ad
sw-yoonsung <sw.yoonsung@gmail.com>
2017-06-29 11:24:46 +0900

    chapter1 section6 array operator까지

---
+++ test/Chapter1_operator_overloading/section6.cpp
@@ -0,0 +1,116 @@
+//
+// Created by yoonsung-mac on 29/06/2017.
+//
+
+#include <iostream>
+using namespace std;
+
+class FuncObject //c++ 에서 struct ,  class 둘다 같음. 차이는 기본 필드속성이 public / private
+{
+    int arr[4]={1,2,3,4};
+public:
+    FuncObject(){
+    }
+    void operator()(int arg)const{
+        cout<< "정수 : " << arg << endl;
+    }
+    void operator()(int arg1, int arg2)const{
+        cout<< "정수 : " << arg1 << ", "<< arg2<< endl;
+    }
+    void operator()(int arg1, int arg2, int arg3)const{
+        cout<< "정수 : " << arg1 << ", "<< arg2 << ", "<< arg3<< endl;
+    }
+
+    int operator[](int idx)const{
+        cout<< "called const : ";
+        if(idx < 4){
+            return arr[idx];
+        }else{
+            throw "there is no value of idx";
+        }
+
+    }
+    //&가 안붙으면 안불림 
+    int& operator[](int idx){
+        cout<< "called not const : ";
+        if(idx < 4){
+            return arr[idx];
+        }else{
+            throw "there is no value of idx";
+        }
+
+    }
+};
+
+void Print1(int arg){
+    cout<< "정수 : " << arg << endl;
+}
+void ex1_18(){
+    FuncObject func;
+
+    for(int i=0; i<4 ; i++){
+        cout << func[i] << endl;
+    }
+
+    const FuncObject& func2=func;
+
+    for(int i=0; i<4 ; i++){
+        cout << func2[i] << endl;
+    }
+
+    func[0]=100;
+    for(int i=0; i<4 ; i++){
+        cout << func[i] << endl;
+    }
+
+//    func2[0]=100; //error
+//    for(int i=0; i<4 ; i++){
+//        cout << func2[i] << endl;
+//    }
+}
+void ex1_16(){
+    FuncObject func;
+
+    for(int i=0; i<5 ; i++){
+        cout << func[i] << endl;
+    }
+}
+void ex1_15(){
+    FuncObject print;
+    //객체 생성 후 암시적 호출
+    print(10);
+    print(10, 20);
+    print(10, 20, 30);
+    cout << endl;
+
+    //객체 생성후 명시적 호출
+    print.operator()(10);
+    print.operator()(10, 20);
+    print.operator()(10, 20, 30);
+    cout << endl;
+
+    //임시 객체 암시적 호출
+    FuncObject()(10);
+    FuncObject()(10, 20);
+    FuncObject()(10, 20, 30);
+    cout << endl;
+
+    //임시 객체 명시적 호출
+    FuncObject().operator()(10);
+    FuncObject().operator()(10, 20);
+    FuncObject().operator()(10, 20, 30);
+    cout << endl;
+
+}
+void ex1_14(){
+    void (*Print2)(int) = Print1;
+    FuncObject Print3;
+
+    Print1(10);
+    Print2(10);
+    Print3(10);
+}
+int main(){
+    ex1_18();
+    return 0;
+}
